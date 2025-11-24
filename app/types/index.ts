59d3f606843ff3867dbf10138ce18218b5cdc587
Abhishek Deshmukh <abhishekd@301io.com>
2025-11-24 23:26:52 +0530

    updated

--- app/types/index.ts
+++ app/types/index.ts
@@ -6,7 +6,7 @@ export interface User {
   relativeName: string;
   houseNo: string;      // changed from number → string
   address?: string;     // optional because your data has NO address field
-  age: number;
+  age: string;
   gender: string;
   status?: string;      // optional, since you will add it later (Active/Pending/etc.)
   lastUpdated?: string; // optional, because your JSON does not include this yet
